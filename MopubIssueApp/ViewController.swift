c9ca3822024e7405bf51a4628feb33cfda641e96
Paulo Arthur Venturi <paulo.venturi@gmail.com>
2014-11-11 15:20:48 -0600

    Initial Commit

---
+++ MopubIssueApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MopubIssueApp
+//
+//  Created by Paulo Arthur Venturi on 11/11/14.
+//  Copyright (c) 2014 Paulo A Venturi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
